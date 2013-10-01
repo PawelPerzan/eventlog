a0a1f6a1f62b284e7e7b89ab73f330806c502d9c
PawelPerzan <pawelperzan@hotmail.com>
2013-09-30 23:48:12 -0400

    Sample junit test

---
+++ src/test/java/services/EventServiceTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package services;
+
+import junit.framework.TestCase;
+
+public class EventServiceTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
